a12b3d135711156ac5892debb90c5c1d339e36b0
Rehan <rehantechnical19@gmail.com>
2024-10-10 13:17:25 +0530

    create singin api route

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import { Footer } from 'flowbite-react';
 import { Link } from 'react-router-dom';
 import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
+
 export default function FooterCom() {
   return (
     <Footer container className='border border-t-8 border-teal-500'>
