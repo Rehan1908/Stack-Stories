418f4281a6c238ecb0449bb695a23f2eee88415e
Rehan <rehantechnical19@gmail.com>
2024-10-15 20:52:02 +0530

    complete user api route

--- api/index.js
+++ api/index.js
@@ -2,17 +2,28 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js'
+import authRoutes from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-  .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
@@ -25,8 +36,4 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
-});
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
 });
